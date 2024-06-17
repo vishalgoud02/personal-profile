61debb21f7f8a434ecaea3972707e3f382746e73
vishalgoud02 <77621998+vishalgoud02@users.noreply.github.com>
2024-06-17 20:04:06 +0530

    Add files via upload

--- casestudy/index.php
+++ casestudy/index.php
@@ -5,8 +5,10 @@
   <meta charset="utf-8">
   <link rel="shortcut icon" href="../images/favicon.ico" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <script src="../js/bootstrap.bundle.min.js"></script>
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,23 +16,56 @@
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Sen:wght@400..800&display=swap" rel="stylesheet">
 
-  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" /> -->
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" media="(min-width: 550px)" />
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" media="(min-width: 550px)" /> -->
 
-  <link href="../css/style.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="../css/swiper-bundle.min.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+  <link href="../css/aos.css" rel="stylesheet" media="(min-width: 550px)" />
+
+  
+
+  <link rel="stylesheet" href="../css/swiper-bundle.min.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+  <link href="../css/aos.css" rel="stylesheet" media="(min-width: 550px)" /><link href="../css/style.css" rel="stylesheet" type="text/css" />
   <link href="../css/responsive.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
+<body class="page_404">
+  <header>
+    <div class="bg_txt">Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer Creative UI/UX Designer</div>
+    <div class="top_strip">
+      <nav class="navbar navbar-expand-md navbar-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="../index.php#top"><img src="../images/brand-logo.svg" alt="logo" /></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar"> <span class="navbar-toggler-icon"></span> </button>
+          <div class="collapse navbar-collapse" id="mynavbar">
+            <ul class="navbar-nav ms-auto me-4  ">
+              <li class="nav-item">
+                <a class="nav-link" href="../index.php#top">Home<span>01</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../index.php#about">About<span>02</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../index.php#Portfolio">Portfolio<span>03</span></a>
+              </li>
+            </ul>
+            <a href="mailto:vishalgoud81gmail.com" class="btn btn-dark">Contact</a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
 
   <section class="page-404 burger">
     <div class="container">
       <div class="row justify-content-center align-items-center">
-        <div class="col-sm-5">
-          <img src="" alt="page 404" />
+        <div class="col-sm-6">
+          <img src="../images/page_404.png" alt="page 404" width="ime-fluid" />
         </div>
-        <div class="col-sm-6 offset-sm-1">
+        <div class="col-sm-6">
           <span><span>Error</span> 404</span>
           <h5>there is light in here too.</h5>
           <h3>But the page is missing or you assembled the link incorrectly.</h3>
@@ -40,11 +75,75 @@
     </div>
   </section>
 
-  <!-- <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script> -->
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="footer">
+            <div class="position-relative">
+              <h2> <span>Let’s work</span> <span>together</span></h2>
+              <div class="clear"></div>
+              <div><a href="mailto:vishalgoud81gmail.com" class="round_btn">Get In Touch</a></div>
+              <hr />
+            </div>
+            <div>
+              <a href="mailto:vishalgoud81gmail.com" class="btn">vishalgoud81@gmail.com</a>
+              <a href="tel:+918103496299" class="btn">+91 81034 96299</a>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <span>Updated Year</span>
+            <div>20224</div>
+          </div>
+          <div class="col-sm-6 text-end">
+            <span>Social Connection </span>
+            <ul>
+              <li>
+                <a href="https://www.behance.net/vishalgoud" target="_blank">Behance</a>
+              </li>
+              <li>
+                <a href="https://dribbble.com/vishal_goud" target="_blank">Dribbble</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/vishalkgoud/" target="_blank">Linkedin</a>
+              </li>
+              <li>
+                <a href="#" target="_blank">Facebook</a>
+              </li>
+              <li>
+                <a href="#" target="_blank">Skype</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
+
+  <!-- <script src="./js/swiper-bundle.min.js"></script> -->
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/aos.js"></script>
   <script>
     AOS.init();
+  </script>
+
+  <script type="text/javascript">
+    function topfix(){ /*Top Strip Fix*/
+      $('.top_strip').toggleClass("fixed-top", ($(window).scrollTop() > 500));
+      setTimeout(topfix, 500);
+      $('body').toggleClass("top_fixed", ($(window).scrollTop() > 100));
+      setTimeout(topfix, 500);
+    }
+    topfix();
   </script>
 </body>
 </html>
